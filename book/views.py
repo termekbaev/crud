9bfc0ff35434eca17e2f790c7b75af69f78e2093
Timur <t-ermekbaev@mail.ru>
2025-01-28 20:07:53 +0300

    Views inheritance from ModelViewSet

--- book/views.py
+++ book/views.py
@@ -1,12 +1,10 @@
-from rest_framework.mixins import ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, \
-    DestroyModelMixin
-from rest_framework.viewsets import GenericViewSet
+from rest_framework.viewsets import ModelViewSet
 
 from book.models import Book
 from book.serializers import BookSerializer
 
 
-class BookView(GenericViewSet, ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin):
+class BookView(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     lookup_field = 'pk'
