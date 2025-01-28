2b4676fd259b121da237eccd56e17f491782b48b
Timur <t-ermekbaev@mail.ru>
2025-01-28 20:02:34 +0300

    Views inheritance from GenericViewSet

--- book/urls.py
+++ book/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from book.views import BookListView, BookDetailView
+from book.views import BookView
 
 urlpatterns = [
-    path('', BookListView.as_view()),
-    path('<int:pk>/', BookDetailView.as_view()),
+    path('', BookView.as_view({'get': 'list', 'post': 'create'}), name='book-list'),
+    path('<pk>/', BookView.as_view({'get': 'retrieve', 'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'}), name='book-detail'),
 ]
--- book/views.py
+++ book/views.py
@@ -1,15 +1,12 @@
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.mixins import ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, \
+    DestroyModelMixin
+from rest_framework.viewsets import GenericViewSet
 
 from book.models import Book
 from book.serializers import BookSerializer
 
 
-class BookListView(ListCreateAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-
-
-class BookDetailView(RetrieveUpdateDestroyAPIView):
+class BookView(GenericViewSet, ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     lookup_field = 'pk'
